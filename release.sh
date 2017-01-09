d614bbc107968b30d9aeebfd8c32bf6eecb727e8
jdj <jdj@cabal>
2017-01-08 22:06:31 -0600

    Release.sh added for easy release :)

---
+++ release.sh
@@ -0,0 +1,29 @@
+# Script to release the mod into a zip file.
+# Assumes BABUN on windows or something like that on Linux (duh)
+
+rm -rf tmp
+mkdir tmp
+
+echo "Copying mod files"
+git clone . tmp/yupgi_alert
+rm -rf tmp/yupgi_alert/{.git,assets,OpenRA.Mods.yupgi_alert}
+rm -f tmp/yupgi_alert/.gitignore
+
+# copy the DLL file and the license.
+echo "Copying DLL files and license info"
+cp OpenRA.Mods.yupgi_alert.dll tmp/yupgi_alert
+cp OpenRA.Mods.yupgi_alert/{LICENSE.AS,AUTHORS.AS} tmp/yupgi_alert
+
+# patch mod.yaml
+echo "Patching mod.yaml"
+cd tmp/yupgi_alert
+patch < mod.yaml.patch
+
+# now in tmp dir.
+cd ..
+echo "Archiving into zip"
+zip yupgi_alert.zip -r yupgi_alert
+mv yupgi_alert.zip ..
+cd ..
+
+echo "Done"
