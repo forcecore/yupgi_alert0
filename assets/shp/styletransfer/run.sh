91639d0ead40699fca48629f647cc777f38f8e19
forcecore <ssanzing@gmail.com>
2017-08-02 20:52:52 -0500

    Polishing of Rage generator

--- assets/shp/styletransfer/run.sh
+++ assets/shp/styletransfer/run.sh
@@ -1,10 +1,11 @@
-for f in in/vein*.png ; do
+for f in in/nairon*.png ; do
     echo $f
     g=`basename "$f" .png`
 
-    for style in snow grass desert ; do
+    #for style in snow grass desert ; do
+    for style in ultra ; do
         python3 -u neural_style_transfer.py \
-            --iter=5 \
+            --iter=10 \
             --content_weight=0.0001 \
             --style_weight=5.0 \
             --tv_weight=1.0 \
